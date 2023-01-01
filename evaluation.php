7e45dc835bf39002a650258a02eca02ea6d15f6e
Navellia <ncent4lyfe@gmail.com>
2023-01-02 00:06:14 +0800

    Update evaluation.php

--- evaluation.php
+++ evaluation.php
@@ -7,6 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="css/style.css">
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js">
+    </script>
     <title>Evaluation | Lambe Turah</title>
 </head>
 
@@ -27,6 +30,7 @@
             <?php
             require_once __DIR__ . '/vendor/autoload.php';
             include_once('simple_html_dom.php');
+
             $con = mysqli_connect("localhost", "root", "", "berita");
             $sql = "";
 
@@ -63,19 +67,39 @@
                 }
                 array_push($result_Arr, $status);
                 echo "<tr>";
-                echo "<td>".$title_Arr[$i]."</td>";
-                echo "<td>".$oriCategory_Arr[$i]."</td>";
-                echo "<td>".$classificationRes_Arr[$i]."</td>";
-                echo "<td>".$result_Arr[$i]."</td>";
+                echo "<td>" . $title_Arr[$i] . "</td>";
+                echo "<td>" . $oriCategory_Arr[$i] . "</td>";
+                echo "<td>" . $classificationRes_Arr[$i] . "</td>";
+                echo "<td>" . $result_Arr[$i] . "</td>";
                 echo "</tr>";
             }
 
-            $correctAccuracy = round(($correctCount/$totalData)*100, 2);
-            $wrongAccuracy = round(($wrongCount/$totalData)*100, 2);
-
-            echo $correctAccuracy."<br>";
-            echo $wrongAccuracy."<br>";
+            $correctAccuracy = round(($correctCount / $totalData) * 100, 2);
+            $wrongAccuracy = round(($wrongCount / $totalData) * 100, 2);
+            echo "</tbody>";
             ?>
+
+            <script type="text/javascript">
+                google.charts.load('current', {
+                    'packages': ['corechart']
+                });
+                google.charts.setOnLoadCallback(drawChart);
+
+                function drawChart() {
+                    var data = google.visualization.arrayToDataTable([
+                        ['Result', 'Count'],
+                        <?php
+                            echo "['Correct', ".$correctAccuracy."]," ;
+                            echo "['Wrong', ".$wrongAccuracy."]" ;
+                        ?>
+                        
+                    ]);
+                    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+                    chart.draw(data);
+                }
+            </script>
+
+            <div id="piechart" style="width: 400px; height: 250px; margin: auto; "></div>
         </section>
     </div>
 </body>
